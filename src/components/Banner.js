eb1f77892df70e52093a00073a45815bb4dc4130
Samuel Owen <147935156+Samuel1505@users.noreply.github.com>
2024-12-10 21:02:52 +0100

    fourth commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Product Designer", "UI/UX Designer" ];
+  const toRotate = [ "Product Designer", "UI/UX " ];
   const period = 2000;
 
   useEffect(() => {
